155772d20f8a0a7e2ecb69379a5dbf4c2fb41a86
hzheng <hazelzh522@gmail.com>
2025-04-14 17:58:46 +0000

    debug

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('Name', 'Date', 'Time', 'TimeZone')
+        fields = ('name', 'date', 'time', 'location', 'timeZone')
